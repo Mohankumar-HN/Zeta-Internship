bbceeaadf23e0ee21c281b87e612ad265a4ccc59
Mohan Kumar H N <mohankumarhn@BMDVMOHANKUMARH.local>
2026-02-06 11:45:28 +0530

    resolved the -ve values

---
+++ Feb6/src/main/java/com/zeta/BankTasks.java
@@ -0,0 +1,43 @@
+package com.zeta;
+
+class Withdrawtask implements Runnable{
+    private final BankAccount account;
+    private final int amount;
+
+     public Withdrawtask(BankAccount account,int amount) {
+        this.account = account;
+        this.amount=amount;
+    }
+    @Override
+    public void run() {
+            account.validateAmount(amount);
+            String thread=Thread.currentThread().getName();
+            boolean success = account.withdraw(amount);
+            if (success) {
+                System.out.println(thread +"withdrawn amount  sussessfully :" + amount);
+            } else {
+                System.out.println("Insufficient balance");
+            }
+    }
+}
+
+class DepositTask implements Runnable {
+    private final BankAccount account;
+    private final int amount;
+
+
+    public DepositTask(BankAccount account, int amount) {
+        this.account = account;
+        this.amount = amount;
+    }
+
+    @Override
+    public void run() {
+        account.validateAmount(amount);
+            String thread=Thread.currentThread().getName();
+            account.deposit(amount);
+            System.out.println(thread+"Depositing money...");
+            System.out.println("Deposit completed \n Remaining balance : " + account.getBalance()
+            );
+    }
+}
---
+++ Feb6/src/main/java/com/zeta/Main.java
@@ -0,0 +1,64 @@
+package com.zeta;
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter initial balance :");
+        int initialbalance=sc.nextInt();
+        while(initialbalance<0){
+            System.out.println("enter valid balance");
+            initialbalance=sc.nextInt();
+        }
+        BankAccount account=new BankAccount(initialbalance);
+        ExecutorService executor=Executors.newFixedThreadPool(3);
+        while(true){
+            System.out.println("\nMULTITHREADED BANKING SYSTEM");
+            System.out.println("1. Check Balance");
+            System.out.println("2. Deposit Money");
+            System.out.println("3. Withdraw Money");
+            System.out.println("4. Simulate Parallel Withdrawals");
+            System.out.println("5. Exit");
+            System.out.print("Enter your choice: ");
+
+            int choice=sc.nextInt();
+            switch (choice){
+                case 1:
+                    System.out.println("Current balance :"+account.getBalance());
+                    break;
+
+                case 2:
+                    System.out.println("Enter amount to deposit");
+                    int amount=sc.nextInt();
+                    executor.execute(new DepositTask(account,amount));
+                    break;
+
+                case 3:
+                    System.out.println("Enter amount to withdraw:");
+                    int wamount=sc.nextInt();
+                    executor.execute(new Withdrawtask(account,wamount));
+                    break;
+
+                case 4:
+                    System.out.println("Simulating two parallel withdrawals of ₹" + (initialbalance / 2));
+
+                    executor.execute(new Withdrawtask(account, initialbalance / 2));
+                    executor.execute(new Withdrawtask(account, initialbalance / 2));
+                    break;
+
+                case 5:
+                    System.out.println("Shutting down");
+                    executor.shutdown();
+                    sc.close();
+                    System.exit(0);
+                    break;
+
+                default:
+                    System.out.println("Invalid choice! Try again.");
+            }
+        }
+
+    }
+}
